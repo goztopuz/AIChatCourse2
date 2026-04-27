ff98d0952e7539bc3b7e62c407ded44b1d839a95
gokhanoztopuz <gokhanoztopuz@gmail.com>
2026-04-27 16:37:39 +0300

    Initial Commit

---
+++ AIChatCourse/AIChatCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatCourseApp.swift
+//  AIChatCourse
+//
+//  Created by Gokhan OZTOPUZ on 27.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct AIChatCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
